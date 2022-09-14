1d1b3e2bc9b59aad250639b23fde5cd3c3508da7
Salim66 <salimhasanriad8@gmail.com>
2022-09-14 11:57:56 +0600

    Managing Transition and Appering Animation

--- user/ecom/src/index.js
+++ user/ecom/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './assets/css/custom.css';
 import './assets/css/fontawesome.css';
+import './assets/css/animate.min.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
