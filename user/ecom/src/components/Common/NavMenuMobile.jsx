ecc0fd89d23091c060b67bea4b2b854adb3dd422
Salim66 <salimhasanriad8@gmail.com>
2022-09-15 17:18:33 +0600

    Nav Mobile View Menu part 2

--- user/ecom/src/components/Common/NavMenuMobile.jsx
+++ user/ecom/src/components/Common/NavMenuMobile.jsx
@@ -4,6 +4,35 @@ import { Link } from 'react-router-dom';
 import Logo from '../../assets/images/easyshop.png';
 
 export class NavMenuMobile extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      sideNavState: "sideNavClose",
+      contentOverState: "ContentOverlayClose"
+    }
+  }
+
+
+  menuBarClickHandler = () => {
+    this.sideNavOpenClose();
+  }
+
+  contentOverlayClickHandler = () => {
+    this.sideNavOpenClose();
+  }
+
+  sideNavOpenClose = () => {
+    let sideNavState = this.state.sideNavState;
+    let contentOverState = this.state.contentOverState;
+
+    if(sideNavState === "sideNavOpen"){
+      this.setState({ sideNavState: "sideNavClose", contentOverState: "ContentOverlayClose" });
+    }else {
+      this.setState({ sideNavState: "sideNavOpen", contentOverState: "ContentOverlayOpen" });
+    }
+  }
+
   render() {
     return (
       <>
@@ -12,7 +41,7 @@ export class NavMenuMobile extends Component {
             <Row>
 
               <Col xl={4} lg={4} md={4} sm={12} xm={12}>
-                <Button type="button" className='btn'><i className='fa fa-bars'></i></Button>
+                <Button type="button" onClick={ this.menuBarClickHandler } className='btn'><i className='fa fa-bars'></i></Button>
                 <Link to='/'><img className='nav-logo' src={Logo} alt="easylogo" /></Link>
                 <Button type="button" className='cart-btn'><i className='fa fa-shopping-cart'></i> 3 Items </Button>
               </Col>
@@ -20,14 +49,14 @@ export class NavMenuMobile extends Component {
             </Row>
           </Container>
 
-          <div className='sideNavOpen'>
+          <div className={ this.state.sideNavState }>
             <hr className='w-80' />
             <div className="list-group">
               <a href="#" className='list-group-item nav-itemmenu list-group-item-action'><i className='fa fa-home mr-2'> Home </i></a>
             </div>
           </div>
 
-          <div className='ContentOverlayOpen'>
+          <div onClick={ this.contentOverlayClickHandler } className={ this.state.contentOverState }>
 
           </div>
 
