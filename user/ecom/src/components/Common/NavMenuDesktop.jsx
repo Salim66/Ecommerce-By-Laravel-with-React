4c32e430abf8f1165318bde424ea69a6a6bf0ce5
Salim66 <salimhasanriad8@gmail.com>
2022-09-15 15:27:22 +0600

    Nav Menu Desktop Part 1

--- user/ecom/src/components/Common/NavMenuDesktop.jsx
+++ user/ecom/src/components/Common/NavMenuDesktop.jsx
@@ -1,9 +1,26 @@
 import React, { Component } from 'react'
+import { Navbar, Container, Row, Col } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/images/easyshop.png';
 
 export class NavMenuDesktop extends Component {
   render() {
     return (
-      <div>NavMenuDesktop</div>
+      <>
+        <div className="TopSectionDown">
+          <Navbar fixed={'top'} bg="light" className="navbar">
+            <Container fluid={true}>
+              <Row>
+                <Col xl={4} lg={4} md={4} sm={12} xm={12}>
+                  <Link to='/'><img className='nav-logo' src={Logo} alt="easylogo" /></Link>
+                </Col>
+                <Col xl={4} lg={4} md={4} sm={12} xm={12}></Col>
+                <Col xl={4} lg={4} md={4} sm={12} xm={12}></Col>
+              </Row>
+            </Container>
+          </Navbar>
+        </div>
+      </>
     )
   }
 }
--- user/ecom/src/pages/HomePage.jsx
+++ user/ecom/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import NavMenuDesktop from '../components/Common/NavMenuDesktop';
 import Categories from '../components/Home/Categories';
 import Collection from '../components/Home/Collection';
 import FeaturedProducts from '../components/Home/FeaturedProducts';
@@ -9,11 +10,12 @@ export class HomePage extends Component {
   render() {
     return (
       <>
+        <NavMenuDesktop />
         <HomeTop />
         <FeaturedProducts />
         <NewArrival />
-        <Collection />
         <Categories />
+        <Collection />
       </>
     )
   }
