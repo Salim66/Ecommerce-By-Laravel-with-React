03ccfc7e9b0176fe70097f1376830e82f5d434dd
Salim66 <salimhasanriad8@gmail.com>
2022-09-18 12:08:55 +0600

    Consume Site info API in Client Side Part 1

--- user/ecom/src/api/AppURL.jsx
+++ user/ecom/src/api/AppURL.jsx
@@ -5,6 +5,7 @@ class AppURL {
     static baseURL = "http://localhost:8000/api";
     static visitorDetails = this.baseURL+"/get-visitor";
     static postContact = this.baseURL+"/post-contact";
+    static AllSiteInfo = this.baseURL+"/get-site-info";
 }
 
 // export default AppURL
--- user/ecom/src/components/others/About.jsx
+++ user/ecom/src/components/others/About.jsx
@@ -1,7 +1,30 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+import AppURL from '../../api/AppURL';
 
 class About extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            about: ''
+        }
+    }
+
+    componentDidMount(){
+
+        axios.get(AppURL.AllSiteInfo)
+        .then(res => {
+            if(res.status == 200){
+                let json_data = (res.data)[0]['about'];
+                this.setState({ about: json_data });
+            }
+        })
+        .catch()
+
+    }
+
   render() {
     return (
         <Container>
@@ -9,12 +32,7 @@ class About extends Component {
                 <Col className='shadow-sm bg-white mt-2' lg={12} md={12} sm={12} xs={12}>
                     <h4 className='section-title-login'>About Us</h4>
                     <p className="section-title-contact">
-                        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:<br/><br/>
-
-                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”<br/>
-                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.<br/><br/>
-
-                        The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.<br/>
+                        { this.state.about }
                     </p>
                 </Col>
             </Row>
