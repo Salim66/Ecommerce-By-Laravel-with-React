6560f2bb2e9045bdee312966625149a80540541b
Salim66 <salimhasanriad8@gmail.com>
2022-09-18 20:18:06 +0600

    Consume Category Subcategory API Part 2

--- user/ecom/src/components/Home/Categories.jsx
+++ user/ecom/src/components/Home/Categories.jsx
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import axios from 'axios';
+import AppURL from '../../api/AppURL';
 
 class Categories extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      catList : []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(AppURL.AllCategoryDetails)
+    .then(res => {
+      this.setState({ catList: res.data });
+    })
+    .catch(error => {
+      console.log('error');
+    })
+  }
+
+
   render() {
+
+    let catList = this.state.catList;
+
     return (
       <>
         <Container className='text-center' fluid={true}>
@@ -14,154 +38,22 @@ class Categories extends Component {
 
 
           <Row>
-            <Col key={1} xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Top Offers</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Grocery</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Mobiles</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Fashion</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-            <Col key={1} xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Electronics</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Appliances</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Travel</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Beauty</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
+            {
+              catList.map((catList, i) => (
+                <>
+                  <Col key={i.toString()} className="p-0" xl={2} lg={2} md={2} sm={6} xs={6}>
+                    <Card className="h-100 w-100 text-center">
+                      <Card.Body>
+                        <img className='center' src={ catList.category_image } alt="product-image" />
+                        <h5 className='category-name'>{ catList.category_name }</h5>
+                      </Card.Body>
+                    </Card>
+                  </Col>
+                </>
+              ))
+            }
           </Row>
-          
-          <Row>
-            <Col key={1} xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/f15c02bfeb02d15d.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Top Offers</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/29327f40e9c4d26b.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Grocery</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/22fddf3c7da4c4f4.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Mobiles</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/c12afc017e6f24cb.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Fashion</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-            <Col key={1} xl={6} lg={6} md={12} sm={12} xs={12}>
-              <Row>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/69c6589653afdb9a.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Electronics</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/0ff199d1bd27eb98.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Appliances</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/71050627a56b4693.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Travel</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-                <Col className="p-0" key={1} xl={3} lg={3} md={3} sm={6} xs={6}>
-                  <Card className="h-100 w-100 text-center">
-                    <Card.Body>
-                      <img className='center' src="https://rukminim1.flixcart.com/flap/128/128/image/dff3f7adcf3a90c6.png?q=100" alt="product-image" />
-                      <h5 className='category-name'>Beauty</h5>
-                    </Card.Body>
-                  </Card>
-                </Col>
-              </Row>
-            </Col>
-          </Row>
+
           
         </Container>
       </>
--- user/ecom/src/components/Home/MegaMenu.jsx
+++ user/ecom/src/components/Home/MegaMenu.jsx
@@ -3,29 +3,24 @@ import React, { Component } from 'react'
 export class MegaMenu extends Component {
 
   constructor(props){
-    super(props);
+    super();
   }
 
 
-  megaMenu(){
-    let acc = document.getElementsByClassName("accordion");
-    let acc_length = acc.length;
-    for (let i = 0; i < acc_length; i++) {
-      acc[i].addEventListener('click', function(){
-        this.classList.toggle("active");
-        let panel = this.nextElementSibling;
-        if(panel.style.maxHeight){
-          panel.style.maxHeight = null;
-        }else {
-          panel.style.maxHeight = panel.style.scrolHeight + "px";
-        }
-      });      
+  menuItemClick = (e) => {
+    e.target.classList.toggle("active");
+    var panel = e.target.nextElementSibling;
+    if(panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    }else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
     }
   }
 
   render() {
 
     let catList = this.props.data;
+
 
     return (
       <>
@@ -35,14 +30,19 @@ export class MegaMenu extends Component {
           {
             catList.map((catList, i) => (
               <div key={i.toString()}>
-                <button className='accordion'>
+                <button onClick={ this.menuItemClick } className='accordion'>
                   <img className='accordionMenuIcon' src={ catList.category_image } alt="" />&nbsp;
                   { catList.category_name }
                 </button>
                 <div className='panel'>
                   <ul>
-                    <li><a className='accordionItem' href="#">Man's Tshirt 1</a></li>
-                    <li><a className='accordionItem' href="#">Man's Tshirt 1</a></li>
+                    {
+                      (catList.subcategory).map((sub, i) => (
+                        <div key={i.toString()}>
+                          <li><a className='accordionItem' href="#">{ sub.subcategory_name }</a></li>
+                        </div>
+                      ))
+                    }                    
                   </ul>
                 </div>
               </div>
