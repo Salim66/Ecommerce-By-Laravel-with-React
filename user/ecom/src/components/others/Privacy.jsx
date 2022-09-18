6455cff9dd16762f3bb054542f93e7d4ce500782
Salim66 <salimhasanriad8@gmail.com>
2022-09-18 14:53:36 +0600

    Consume Site info API in Client Side Part 5

--- user/ecom/src/components/others/Privacy.jsx
+++ user/ecom/src/components/others/Privacy.jsx
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Button, Form } from 'react-bootstrap';
+import axios from 'axios';
+import AppURL from '../../api/AppURL';
+import parse from 'html-react-parser';
 
 class Privacy extends Component {
+
+    constructor(){
+        super();
+        this.state = {
+            privacy: '',
+            loaderDiv: '',
+            mainDiv: 'd-none'
+        }
+    }
+
+    componentDidMount(){
+
+        axios.get(AppURL.AllSiteInfo)
+        .then(res => {
+            if(res.status == 200){
+                let json_data = (res.data)[0]['privacy'];
+                this.setState({ privacy: json_data, loaderDiv: 'd-none', mainDiv: '' });
+            }
+        })
+        .catch()
+
+    }
+
+
   render() {
     return (
         <Container>
             <Row className='p-2'>
                 <Col className='shadow-sm bg-white mt-2' lg={12} md={12} sm={12} xs={12}>
-                    <h4 className='section-title-login'>Privacy Policy</h4>
-                    <p className="section-title-contact">
-                        Lorem ipsum, or lipsum as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. It usually begins with:<br/><br/>
 
-                        “Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.”<br/>
-                        The purpose of lorem ipsum is to create a natural looking block of text (sentence, paragraph, page, etc.) that doesn't distract from the layout. A practice not without controversy, laying out pages with meaningless filler text can be very useful when the focus is meant to be on design, not content.<br/><br/>
+                    <div className={ this.state.loaderDiv }>
+                        <div class="ph-item">
+                            <div class="ph-col-12">
+                                <div class="ph-row">
+                                    <div class="ph-col-4"></div>
+                                    <div class="ph-col-8 empty"></div>
+                                    <div class="ph-col-6"></div>
+                                    <div class="ph-col-6 empty"></div>
+                                    <div class="ph-col-12"></div>
+                                    <div class="ph-col-12"></div>
+                                    <div class="ph-col-6"></div>
+                                    <div class="ph-col-12"></div>
+                                    <div class="ph-col-12"></div>
+                                    <div class="ph-col-12"></div>
+                                    <div class="ph-col-12"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
 
-                        The passage experienced a surge in popularity during the 1960s when Letraset used it on their dry-transfer sheets, and again during the 90s as desktop publishers bundled the text with their software. Today it's seen all around the web; on templates, websites, and stock designs. Use our generator to get your own, or read on for the authoritative history of lorem ipsum.<br/>
-                    </p>
+                    <div className={ this.state.mainDiv }>
+                        <h4 className='section-title-login'>Privacy Policy</h4>
+                        <p className="section-title-contact">
+                            { parse(this.state.privacy) }
+                        </p>
+                    </div>
+
                 </Col>
             </Row>
         </Container>
