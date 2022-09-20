1eb192116a430100e270886f1a80e82178d6797d
Salim66 <salimhasanriad8@gmail.com>
2022-09-20 11:20:05 +0600

    Consume API For Home Slider for Mobile

--- user/ecom/src/components/Home/HomeTopMobile.jsx
+++ user/ecom/src/components/Home/HomeTopMobile.jsx
@@ -1,16 +1,36 @@
 import React, { Component } from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
+import AppURL from '../../api/AppURL';
 import HomeSlider from './HomeSlider';
-import MegaMenu from './MegaMenu';
+import axios from 'axios';
+
 
 export class HomeTopMobile extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      sliderData: []
+    }
+  }
+
+  componentDidMount(){
+    axios.get(AppURL.AllSlider)
+    .then(res => {
+      this.setState({ sliderData: res.data });
+    })
+    .catch(error => {
+      console.log('error');
+    })
+  }
+
   render() {
     return (
       <>
         <Container className="p-0 m-0 overflow-hidden" fluid={true}>
             <Row className="p-0 m-0 overflow-hidden">
                 <Col lg={12} md={12} sm={12}>
-                    {/* <HomeSlider /> */}
+                    <HomeSlider sliderData = {this.state.sliderData} />
                 </Col>
             </Row>
         </Container>
