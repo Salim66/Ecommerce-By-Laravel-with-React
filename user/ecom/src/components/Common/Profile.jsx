588c90f015d4287a4814064ba9107bae0e95de34
Salim66 <salimhasanriad8@gmail.com>
2022-10-06 18:50:15 +0600

    Update Profile Page

--- user/ecom/src/components/Common/Profile.jsx
+++ user/ecom/src/components/Common/Profile.jsx
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
 import { Navigate } from 'react-router-dom';
+import { Container, Row, Col, Card, ListGroup, ListGroupItem } from 'react-bootstrap';
+import Ariyan from '../../assets/images/kaziariyan.png';
+import { Link } from 'react-router-dom';
 
 class Profile extends Component {
   render() {
@@ -18,11 +21,30 @@ class Profile extends Component {
 
     return (
       <>
-        <h2>User Profile Page</h2>
-        <ul className="list-group">
-            <li className="list-group-item">Name: { name }</li>
-            <li className="list-group-item">Email: { email }</li>
-        </ul>
+       <Container>
+
+          <div className='section-title text-center mb-55'>
+            <h2>Your Profile</h2>
+          </div>
+
+          <Row>
+            <Col lg={4} md={4} sm={12}>
+              <Card>
+                <img src={ Ariyan } alt="" className='userProfile' />
+                <ListGroup>
+                  <ListGroupItem><Link to="/orderlist" className='text-left'>Order List</Link></ListGroupItem>
+                </ListGroup>
+              </Card>
+            </Col>
+            <Col lg={8} md={8} sm={12}>
+              <ul className="list-group">
+                  <li className="list-group-item">Name: { name }</li>
+                  <li className="list-group-item">Email: { email }</li>
+              </ul>
+            </Col>
+          </Row>
+
+        </Container>
       </>
     )
   }
