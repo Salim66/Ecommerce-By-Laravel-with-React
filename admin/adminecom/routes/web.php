2cefc4e83b93d41bcca91b217d0c5445b9c656a8
Salim66 <salimhasanriad8@gmail.com>
2022-10-20 13:00:36 +0600

    Setup Admin Theme

--- admin/adminecom/routes/web.php
+++ admin/adminecom/routes/web.php
@@ -17,12 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])->group(function () {
     Route::get('/dashboard', function () {
-        return view('dashboard');
+        return view('admin.index');
     })->name('dashboard');
 });
