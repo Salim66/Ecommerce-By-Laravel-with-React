118234414d0d8e3f752dc697659ee8fcb7b4f935
Salim66 <salimhasanriad8@gmail.com>
2022-10-20 21:56:17 +0600

    Admin Profile & Image Update Part 2

--- admin/adminecom/resources/views/backend/admin/user_profile.blade.php
+++ admin/adminecom/resources/views/backend/admin/user_profile.blade.php
@@ -36,13 +36,11 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="d-flex flex-column align-items-center text-center">
-                                    <img src="{{ asset('backend/assets') }}/images/avatars/avatar-2.png" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
+                                    <img src="{{ !empty($data->profile_photo_path) ? url('upload/admin_images/'.$data->profile_photo_path) : url('upload/no_image.jpg')  }}" alt="Admin" class="rounded-circle p-1 bg-primary" width="110">
                                     <div class="mt-3">
-                                        <h4>John Doe</h4>
-                                        <p class="text-secondary mb-1">Full Stack Developer</p>
+                                        <h4>{{ $data->name }}</h4>
+                                        <p class="text-secondary mb-1">{{ $data->email }}</p>
                                         <p class="text-muted font-size-sm">Bay Area, San Francisco, CA</p>
-                                        <button class="btn btn-primary">Follow</button>
-                                        <button class="btn btn-outline-primary">Message</button>
                                     </div>
                                 </div>
                                 <hr class="my-4" />
@@ -92,26 +90,10 @@
                                 </div>
                                 <div class="row mb-3">
                                     <div class="col-sm-3">
-                                        <h6 class="mb-0">Phone</h6>
+                                        <h6 class="mb-0">Profile Photo</h6>
                                     </div>
                                     <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(239) 816-9029" />
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Mobile</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="(320) 380-4539" />
-                                    </div>
-                                </div>
-                                <div class="row mb-3">
-                                    <div class="col-sm-3">
-                                        <h6 class="mb-0">Address</h6>
-                                    </div>
-                                    <div class="col-sm-9 text-secondary">
-                                        <input type="text" class="form-control" value="Bay Area, San Francisco, CA" />
+                                        <input type="file" class="form-control" name="profile_photo_path" />
                                     </div>
                                 </div>
                                 <div class="row">
